d914b87973c0db9bb3e3535c22298466547a9d58
Mohit Singh <mohitsingh1930@gmail.com>
2020-03-25 08:47:05 +0530

    dropdowns html and js created and included

---
+++ public/js/renderDepartments.js
@@ -0,0 +1,12 @@
+
+function renderDepartments(response) {
+	let departmentDropdowns = document.querySelectorAll(".dropdown-department");
+
+	for(let dropdown of departmentDropdowns) {
+		for(let department of response.list) {
+			dropdown.insertAdjacentHTML("beforeend", `<option value="${department}">${department}</option>`)
+		}
+	}
+}
+
+load("POST", "/departments", renderDepartments);
